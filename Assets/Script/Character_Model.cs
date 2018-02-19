788e68032a1e42cb20f5578987e8a1f12107140c
稲垣和真 <ik@IK.local>
2018-02-19 17:12:04 +0900

    inital commit

---
+++ Assets/Script/Character_Model.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModel {
+	int HP;//ヒットポイント
+	int MP;//マジックポイント
+	int AT;//攻撃力
+	int DE;//防御力
+	string serifu;//会心の一撃のセリフ
+	string name;//キャラの名前
+	string ziseinoku;//負けた時のセリフ
+	bool sleepFlg;
+	bool mahiFlg;
+	bool poizonFlg;
+	bool[] status;//状態
+	int refCount;
+	bool refectFrg;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Text log;
+
+	// Use this for initialization
+	void Start () {
+		SetLogClean ("114514");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//ログに反映させるメソッド
+	void SetLog(string txt){
+		log.text = log.text + txt;
+	}
+    //純正なセッター
+	void SetLogClean(string txt){
+		log.text = txt;
+	}
+
+	void SetLogAdd(string txt){
+		log.text = log.text + txt + "\n";
+	}
+}
